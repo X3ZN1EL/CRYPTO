e737d9fefe1a1cf11d5f7da1ff6a75bf1e20478f
Alejandro Jacobo <72319598+X3ZN1EL@users.noreply.github.com>
2021-05-22 17:18:25 -0500

    Update PIA-CRYPTO-2021.py

--- misc/PIA-CRYPTO-2021.py
+++ misc/PIA-CRYPTO-2021.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3 
-# xen was here
+#!/usr/bin/env python3
 
 import random
 from base64 import b64encode, b64decode
@@ -9,6 +8,14 @@ from Cryptodome.Random import get_random_bytes
 from Crypto.Util.number import *
 from Crypto.PublicKey import RSA
 import binascii
+
+# VALIDAR INPUT
+def validar(response):
+    try:
+        temp = int(response)
+        return True
+    except:
+        pass
 
 # KEYS GENERATION
 # openssl genrsa -out private-key.pem 2048
@@ -41,37 +48,38 @@ def decrypt_envelope(cipher_key):
     clear_key = long_to_bytes(message).decode()
     return clear_key
 
-# RANDOM KEY
-key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-
 # ENCRYPT AES
-def encrypt(plain_text, key):
+def encrypt(plain_text):
+    # RANDOM KEY
+    key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16)).encode('utf-8')
     # Genera un random salt
     salt = get_random_bytes(AES.block_size)
     # Usa un KDF para generar la llave
     private_key = hashlib.scrypt(
-        key.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+        key, salt=salt, n=2**14, r=8, p=1, dklen=32)
     # Inicia el modo de operacion
     cipher_config = AES.new(private_key, AES.MODE_GCM)
     # Crea el dic cifrado
     cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
     return {
         'cipher_text': b64encode(cipher_text).decode('utf-8'),
-        'key': b64encode(salt).decode('utf-8'),
+        'key': b64encode(key).decode(),
+        'salt': b64encode(salt).decode('utf-8'),
         'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
         'tag': b64encode(tag).decode('utf-8')
     }
 
 # DECRYPT AES
-def decrypt(enc_dict, key):
+def decrypt(enc_dict,key):
     # Hace el decode del dic cifrado
-    salt = b64decode(enc_dict['key'])
+    k = b64decode(key)
+    salt = b64decode(enc_dict['salt'])
     cipher_text = b64decode(enc_dict['cipher_text'])
     nonce = b64decode(enc_dict['nonce'])
     tag = b64decode(enc_dict['tag'])
     # Genera un salt en la llave
     private_key = hashlib.scrypt(
-        key.encode(), salt=salt, n=2**14, r=8, p=1, dklen=32)
+        k, salt=salt, n=2**14, r=8, p=1, dklen=32)
     # Inicia el modo de operacion
     cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
     # Hace el decrypt
@@ -79,43 +87,47 @@ def decrypt(enc_dict, key):
     return decrypted
 
 # CARTA 
-message1 = input("carta: ")
-message1_enc_json = encrypt(message1,key)
+message = input("carta: ")
+message_enc_json = encrypt(message)
+
+# AES DATA
+aes_msg = message_enc_json['cipher_text']
+aes_key = message_enc_json['key']
 
 # CARTA CIFRADA
 print("\nCarta cifrada:")
-ciphertext = message1_enc_json['cipher_text']
-print(ciphertext)
+print(aes_msg)
 
-# AES KEY CIFRADA
+# AES KEY ENVELOPE
 print("\nEnvelope: ")
-aes_key_envelope= message1_enc_json['key']
-result_key_envelope = rsa_envelope(aes_key_envelope.encode())
-print(result_key_envelope)
-
-# USER INTERACTION
+rsa_enc_key = rsa_envelope(aes_key.encode())
+print(rsa_enc_key)
 
 response_user = input("\nQuieres saber que dice?\ns/n\n -> ")
-if response_user == "s":
+
+if response_user == str('s'):
     print("\nResponde la pregunta correctamente")
     num1 = random.randint(1,10000)
     num2 = random.randint(1,10000)
-    seg_q = int(input("Cuanto es " + str(num1) + " + " +str(num2) + " ? \n -> "))
 
-# SECURITY QUESTION
-    if seg_q == num1 + num2:
-        print("\nRespuesta correcta!") 
-        envelope_verification = int(input("Ingresa el envelope\n -> "))
+    aritmetic_quest = input("Cuanto es " + str(num1) + " + " + str(num2) + " ? \n -> ")
 
-        # ENVELOPE VERIFICATION
-        if envelope_verification == result_key_envelope:
+    if validar(aritmetic_quest):
+
+        if int(aritmetic_quest) == num1 + num2:
             print("\nRespuesta correcta!")
-            dec_carta = decrypt(message1_enc_json,key)
-            print("Carta: "  + dec_carta.decode())
+            envelope_verification = int(input("Ingresa el envelope\n -> "))
+
+            if validar(envelope_verification):
+                if int(envelope_verification) == rsa_enc_key:
+                    print("\nRespuesta correcta!")
+                    key_decrypt = decrypt_envelope(int(envelope_verification))
+                    print("\nCarta: ")
+                    carta_dec = decrypt(message_enc_json,key_decrypt).decode()
+                    print(carta_dec)
+                else:
+                    print("nope!")
         else:
-            print("nope!") 
-    else:
-        print("nope!")
-    
-elif response_user == "n":
-    print("bye!")
+            print("nope!")
+else:
+    print("Bye!")
